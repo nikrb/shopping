c0e485d605774ee0adf709f4133fef7e1c9d9625
nikrb <nik_rb@yahoo.com>
2017-04-07 17:27:09 +0100

    tweak food search layout

--- client/src/components/FoodSearch.js
+++ client/src/components/FoodSearch.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Client from './Client';
 
@@ -7,6 +6,9 @@ const MATCHING_ITEM_LIMIT = 25;
 class FoodSearch extends React.Component {
   state = {
     foods: [],
+    unit: "",
+    amount: "",
+    cost:"",
     showRemoveIcon: false,
     searchValue: '',
   };
@@ -41,19 +43,33 @@ class FoodSearch extends React.Component {
       foods: [],
       showRemoveIcon: false,
       searchValue: '',
+      unit: "",
+      amount: "",
+      cost:""
     });
   };
 
-  handleFoodClick = function( food){
-    this.props.onFoodClick( food);
+  handleFoodClick = () => {
+
+  };
+
+  handleSearchSelect = function( food){
+    // this.props.onFoodClick( food);
+    console.log( "state:", this.state);
     this.handleSearchCancel();
+  };
+  handleAmountChange = (e) => {
+    this.setState( { amount: e.target.value});
+  };
+  handleCostChange = (e) => {
+    this.setState( { cost: e.target.value});
   };
 
   render() {
     const { showRemoveIcon, foods } = this.state;
     const removeIconStyle = showRemoveIcon ? {} : { visibility: 'hidden' };
 
-    // TODO: using ()=>{} creates a new function for onClick every render
+    // FIXME: using ()=>{} creates a new function for onClick every render
     // so just use bind(this, param) syntax
     // try factoring FoodItem out
     const foodRows = foods.map((food, idx) => (
@@ -61,58 +77,55 @@ class FoodSearch extends React.Component {
         key={idx}
         onClick={this.handleFoodClick.bind(this, food)}
       >
-        <td>{food.name}</td>
-        <td className='right aligned'>{food.units}</td>
+        <td className="left-align">{food.name}</td>
+        <td>{food.units}</td>
       </tr>
     ));
 
     return (
       <div id='food-search'>
-        <table>
-          <thead>
-            <tr>
-              <th colSpan='5'>
-                <div>
-                  <div>
-                    <input
-                      type='text'
-                      placeholder='Search foods...'
-                      value={this.state.searchValue}
-                      onChange={this.handleSearchChange}
-                    />
-                    <i className='search icon' />
-                    <i
-                      className='remove icon'
-                      onClick={this.handleSearchCancel}
-                      style={removeIconStyle}
-                    />
-                    <select>
-                      <option value="Kg">Kg</option>
-                      <option value='unit'>Unit</option>
-                    </select>
-                    <input type='text'
-                      className='entry-narrow'
-                      value={this.state.amount}
-                      onChange={this.handleAmountChange}
-                    />
-                    <input type='text'
-                      className='entry-narrow'
-                      value={this.state.cost}
-                      onChange={this.handleCostChange}
-                    />
-                  </div>
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th>Description</th>
-              <th>Units</th>
-            </tr>
-          </thead>
-          <tbody>
-            {foodRows}
-          </tbody>
-        </table>
+        <div>
+          <input
+            type='text'
+            placeholder='Search foods...'
+            value={this.state.searchValue}
+            onChange={this.handleSearchChange}
+          />
+          <i className='search icon' />
+          <i
+            className='remove icon'
+            onClick={this.handleSearchCancel}
+            style={removeIconStyle}
+          />
+          <select onChange={this.handleUnitChange} value={this.state.unit}>
+            <option value="Kg">Kg</option>
+            <option value='unit'>Unit</option>
+          </select>
+          <input type='text'
+            className='entry-narrow'
+            value={this.state.amount}
+            onChange={this.handleAmountChange}
+          />
+          <input type='text'
+            className='entry-narrow'
+            value={this.state.cost}
+            onChange={this.handleCostChange}
+          />
+          <button type="button" onClick={this.handleSearchSelect}>+</button>
+        </div>
+        <div className='food-search-table'>
+          <table>
+            <thead>
+              <tr>
+                <th className="left-align">Description</th>
+                <th>Units</th>
+              </tr>
+            </thead>
+            <tbody>
+              {foodRows}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
