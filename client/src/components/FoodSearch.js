233851b2eb8f651dfb94962a4cdbed1b2c9941fb
nikrb <nik_rb@yahoo.com>
2017-04-08 21:20:18 +0100

    remove unused icons and reformat code

--- client/src/components/FoodSearch.js
+++ client/src/components/FoodSearch.js
@@ -7,64 +7,47 @@ class FoodSearch extends React.Component {
   state = {
     foods: [],
     name: "",
-    unit: "",
+    units: "kg",
     amount: "",
     cost:"",
-    showRemoveIcon: false,
-    searchValue: '',
+    searchValue: ''
   };
 
   handleSearchChange = (e) => {
     const value = e.target.value;
-
     this.setState({
       searchValue: value,
     });
-
     if (value === '') {
-      this.setState({
-        foods: [],
-        showRemoveIcon: false,
-      });
+      this.setState({ foods: []});
     } else {
-      this.setState({
-        showRemoveIcon: true,
-      });
-
       Client.search(value, (foods) => {
-        this.setState({
-          foods: foods.slice(0, MATCHING_ITEM_LIMIT),
-        });
+        this.setState({ foods: foods.slice(0, MATCHING_ITEM_LIMIT)});
       });
     }
   };
 
   handleSearchCancel = () => {
-    this.setState({
-      foods: [],
-      showRemoveIcon: false,
-      searchValue: '',
-      name: "",
-      unit: "",
-      amount: "",
-      cost:""
+    this.setState({ foods: [], searchValue: '',
+      name: "", units: "kg", amount: "", cost:""
     });
   };
 
   handleFoodClick = function( food){
     console.log( "food clicked:", food);
-    this.setState({ name: food.name, searchValue: food.name, unit:food.units}, function(){
+    this.setState({ name: food.name, searchValue: food.name, units:food.units}, function(){
       console.log( "state:", this.state);
     });
     this.entry_amount.focus();
   };
 
   handleSearchSelect = () => {
-    const food = { name: this.state.searchValue, unit: this.state.unit,
+    const food = { name: this.state.searchValue, units: this.state.units,
       amount: parseFloat( this.state.amount), cost: parseFloat( this.state.cost) };
     console.log( "add food:", food);
     this.props.onFoodClick( food);
     this.handleSearchCancel();
+    this.search_input.focus();
   };
   handleAmountChange = (e) => {
     this.setState( { amount: e.target.value});
@@ -77,8 +60,7 @@ class FoodSearch extends React.Component {
   };
 
   render() {
-    const { showRemoveIcon, foods } = this.state;
-    const removeIconStyle = showRemoveIcon ? {} : { visibility: 'hidden' };
+    const { foods } = this.state;
 
     // FIXME: using ()=>{} creates a new function for onClick every render
     // so just use bind(this, param) syntax
@@ -96,34 +78,18 @@ class FoodSearch extends React.Component {
     return (
       <div id='food-search'>
         <div>
-          <input
-            type='text'
-            placeholder='Search foods...'
-            value={this.state.searchValue}
-            onChange={this.handleSearchChange}
-          />
-          <i className='search icon' />
-          <i
-            className='remove icon'
-            onClick={this.handleSearchCancel}
-            style={removeIconStyle}
-          />
-          <select onChange={this.handleUnitChange} value={this.state.unit}>
+          <input type='text' placeholder='Search foods...'
+            value={this.state.searchValue} onChange={this.handleSearchChange}
+            ref={(search_input) => { this.search_input = search_input;}}/>
+          <select onChange={this.handleUnitChange} value={this.state.units}>
             <option value="kg">Kg</option>
             <option value='unit'>Unit</option>
           </select>
-          <input type='text'
-            className='entry-narrow'
-            value={this.state.amount}
-            onChange={this.handleAmountChange}
-            onFocus={this.handleTextFocus}
-            ref={(input) => { this.entry_amount = input;}}
-          />
-          <input type='text'
-            className='entry-narrow'
-            value={this.state.cost}
-            onChange={this.handleCostChange}
-          />
+          <input type='text' className='entry-narrow' value={this.state.amount}
+            onChange={this.handleAmountChange} onFocus={this.handleTextFocus}
+            ref={(input) => { this.entry_amount = input;}} />
+          <input type='text' className='entry-narrow'
+            value={this.state.cost} onChange={this.handleCostChange} />
           <button type="button" onClick={this.handleSearchSelect}>+</button>
         </div>
         <div className='food-search-table'>
