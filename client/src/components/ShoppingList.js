1083df00d7769e167f616e607577e234b11a4869
nikrb <nik_rb@yahoo.com>
2017-04-15 22:00:30 +0100

    allow shopping list date to be changed

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,14 +6,17 @@ import Client from './Client';
 export default class ShoppingList extends Component {
   state = {
     created: null,
-    selectedFoods: [],
+    created_field: "",
+    // date passed in is always valid
+    created_valid: true,
+    selectedFoods: []
   };
   componentWillMount = () => {
     console.log( "shoppingList mount:", this.props.location);
     const { created, selectedFoods} = this.props.location.state.list;
     console.log( "date created:", created);
     // TODO: can we { ...this.props.location.state} here?
-    this.setState( { created: created, selectedFoods: selectedFoods});
+    this.setState( { created: created, created_field: created, selectedFoods: selectedFoods});
   };
   componentWillUnmount = () => {
     Client.postList( { created: this.state.created,
@@ -41,12 +44,30 @@ export default class ShoppingList extends Component {
     }
     this.setState( upd);
   };
+  createdChanged = (e) => {
+    const ts = Date.parse( e.target.value);
+    if( isNaN( ts)){
+      this.setState( { created_field: e.target.value, created_valid: false});
+    } else {
+      this.setState( { created_field: e.target.value, created: e.target.value,
+        created_valid: true});
+    }
+  };
 
   render() {
     const { selectedFoods } = this.state;
+    const field_valid = { "border-color": "green" };
+    const field_invalid = { "border-color": "red" };
+    // FIXME: className = App  could be why we're having trouble
     return (
       <div>
         <h1>Shopping List</h1>
+        <div className="App">
+          <label>Date
+            <input type="text" style={this.state.created_valid?field_valid:field_invalid}
+              value={this.state.created_field} onChange={this.createdChanged} />
+          </label>
+        </div>
         <div className='App'>
           <div className='container'>
             <FoodSearch
