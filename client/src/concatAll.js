bd9949c0bea058a9e9d26d032e8609207b11ce28
nikrb <nik_rb@yahoo.com>
2017-04-19 14:54:25 +0100

    add array concatAll

---
+++ client/src/concatAll.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+    subArray.forEach( function( item){
+      results.push( item);
+    });
+	});
+
+	return results;
+};
