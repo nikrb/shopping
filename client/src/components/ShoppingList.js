18c4eacaa6b3123fca043fcd86770cadee85321a
nikrb <nik_rb@yahoo.com>
2017-04-24 02:32:39 +0100

    only save list if it is updated. style tweaks and warning for created date change

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -13,6 +13,7 @@ export default class ShoppingList extends Component {
     vendor_field: "", // out (no validation)
     selectedFoods: []
   };
+  list_updated: false;
   componentWillMount = () => {
     console.log( "shoppingList mount:", this.props.location);
     const { created, vendor, selectedFoods} = this.props.location.state.list;
@@ -20,21 +21,27 @@ export default class ShoppingList extends Component {
     // TODO: can we { ...this.props.location.state} here?
     this.setState( { created: created, created_field: created,
       vendor: vendor, vendor_field: vendor, selectedFoods: selectedFoods});
+    this.list_updated = false;
   };
   componentWillUnmount = () => {
-    Client.postList( { created: this.state.created,
-      vendor: this.state.vendor_field,
-      selectedFoods: this.state.selectedFoods.map( (food) => {
-        return { name: food.name, units: food.units,
-          amount: food.amount, cost: food.cost};
-      })
-    });
+    if( this.list_updated){
+      const list = { created: this.state.created,
+        vendor: this.state.vendor_field,
+        selectedFoods: this.state.selectedFoods.map( (food) => {
+          return { name: food.name, units: food.units,
+            amount: food.amount, cost: food.cost};
+        })
+      }
+      console.log( "saving list:", list);
+      Client.postList( list);
+    }
   };
   removeFoodItem = (itemIndex) => {
     const filteredFoods = this.state.selectedFoods.filter(
       (item, idx) => itemIndex !== idx,
     );
     this.setState({ selectedFoods: filteredFoods });
+    this.list_updated = true;
   };
 
   addFood = (food) => {
@@ -47,18 +54,22 @@ export default class ShoppingList extends Component {
       Client.postFood( nf);
     }
     this.setState( upd);
+    this.list_updated = true;
   };
   createdChanged = (e) => {
     const ts = Date.parse( e.target.value);
     if( isNaN( ts)){
       this.setState( { created_field: e.target.value, created_valid: false});
+      this.list_updated = false;
     } else {
       this.setState( { created_field: e.target.value, created: e.target.value,
         created_valid: true});
+      this.list_updated = true;
     }
   };
   vendorChanged = (e) => {
     this.setState( { vendor_field: e.target.value});
+    this.list_updated = true;
   };
 
   render() {
@@ -66,20 +77,30 @@ export default class ShoppingList extends Component {
     const field_valid = { borderColor: "green" };
     const field_invalid = { borderColor: "red" };
     // space after label
-    const vendor_margin = { marginLeft:"10px"};
+    const field_margin = { marginLeft:"0.5em"};
+    const notes = {
+      display: "flex", justifyContent: "center",
+      marginTop: "1em",
+      fontSize: "0.5em", fontStyle: "italic"
+    }
     // FIXME: className = App  could be why we're having trouble
     return (
       <div>
         <h1>Shopping List</h1>
         <div className="App">
           <label>Date
-            <input type="text" style={this.state.created_valid?field_valid:field_invalid}
+            <input type="text" style={Object.assign( {},
+                this.state.created_valid?field_valid:field_invalid,
+                field_margin)}
               value={this.state.created_field} onChange={this.createdChanged} />
           </label>
           <label>Vendor
-            <input type="text" value={this.state.vendor_field} style={vendor_margin}
+            <input type="text" value={this.state.vendor_field} style={field_margin}
               onChange={this.vendorChanged} />
           </label>
+        </div>
+        <div style={notes}>
+          *Changing the date will create a duplicate list
         </div>
         <div className='App'>
           <div className='container'>
